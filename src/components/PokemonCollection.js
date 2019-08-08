1629086bd2581f88e3f7153246146ae841bd1de9
JakubowskiA <arieljakubowski@gmail.com>
2019-08-08 15:12:21 -0400

    Done.

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -4,9 +4,21 @@ import { Card } from 'semantic-ui-react'
 
 class PokemonCollection extends React.Component {
   render() {
+    // console.log(this.props.pokemonCollection);
+    
+    let pokemonArray = this.props.pokemonCollection.map(pokemon=>(
+      <PokemonCard 
+      key={pokemon.id}
+      pokemon={pokemon}
+      // handleClick={this.props.handleClick}
+      />
+    ))
+    // console.log("array", pokemonArray);
+    
     return (
       <Card.Group itemsPerRow={6}>
         <h1>Hello From Pokemon Collection</h1>
+        {pokemonArray}
       </Card.Group>
     )
   }
--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -5,16 +5,46 @@ import { Search } from 'semantic-ui-react'
 import _ from 'lodash'
 
 class PokemonPage extends React.Component {
+  state={
+    pokemonCollection:[],
+    searchInput:""
+  }
+
+  // handleClick = () => {
+  //   const index = this.state.pokemonCollection
+  //   console.log(index);
+    
+  // }
+  onSearchChange= event=>{ 
+    
+    this.setState(
+      {searchInput: event.target.value }, 
+    () => console.log(this.state.searchInput)) 
+  
+  }
+
+  addPokemon = pokemon => {
+    this.setState({ pokemonCollection: [...this.state.pokemonCollection, pokemon] })
+  }
+
+
+  componentDidMount(){
+    fetch('http://localhost:3000/pokemon')
+    .then(response => response.json())
+    .then(pokemonCollection => this.setState({pokemonCollection}))
+  }
   render() {
+    const targetPokemon = this.state.pokemonCollection.filter(pokemon => pokemon.name.includes(this.state.searchInput))
+    console.log(targetPokemon)
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+        <Search onSearchChange={this.onSearchChange} showNoResults={false} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemonCollection = {targetPokemon} />
         <br />
-        <PokemonForm />
+        <PokemonForm addPokemon={this.addPokemon}/>
       </div>
     )
   }
