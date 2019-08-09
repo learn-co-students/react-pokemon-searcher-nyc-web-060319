117041ff230d8ed467bbd414916aff9c8f16ef2c
anmester <github email address>
2019-08-09 10:02:21 -0400

    Done.

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -3,10 +3,15 @@ import PokemonCard from './PokemonCard'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCollection extends React.Component {
+
+  
   render() {
+    // console.log('this is state', this.state.pokemons)
+    let pokeArray = this.props.pokemons.map( pokeObj => <PokemonCard key={pokeObj.id} pokemon={pokeObj} />)
     return (
       <Card.Group itemsPerRow={6}>
         <h1>Hello From Pokemon Collection</h1>
+        {pokeArray}
       </Card.Group>
     )
   }
--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -5,16 +5,38 @@ import { Search } from 'semantic-ui-react'
 import _ from 'lodash'
 
 class PokemonPage extends React.Component {
+  
+  state = {
+    pokemons: [],
+    searchValue: ''
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/pokemon')
+    .then(res => res.json())
+    .then(pokemonData => this.setState({pokemons: pokemonData}))
+  }
+
+  filterSearch = event => {
+    this.setState({searchValue: event.target.value})
+    // console.log(this.state)
+  }
+
+  addPokemon = pokemon => {
+    this.setState({ pokemons: [...this.state.pokemons, pokemon] })
+  }
+  
   render() {
+    let targetPokemon = this.state.pokemons.filter(pokemon => pokemon.name.includes(this.state.searchValue))
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+        <Search onSearchChange={this.filterSearch} showNoResults={false} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemons={targetPokemon}/>
         <br />
-        <PokemonForm />
+        <PokemonForm addPokemon={this.addPokemon}/>
       </div>
     )
   }
